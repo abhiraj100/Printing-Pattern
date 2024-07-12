9b2b6048b6f77f8384ab0fc7a8644a7225c625a7
ABHIRAJ YADAV <102871390+abhiraj100@users.noreply.github.com>
2024-07-13 00:20:39 +0530

    Pattern-9

---
+++ Pattern9.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Pattern9 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        /*
+        int i = 1;
+        while (i<=n) {
+            int j = 1;
+            while (j <= i) {
+                System.out.print(i+j-1);
+
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+         */
+        int i = 1;
+        while (i<=n) {
+            int j = 1;
+            int startingNumber =i;
+            while (j <= i) {
+                System.out.print(startingNumber);
+                startingNumber++;
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+
+    }
+}
